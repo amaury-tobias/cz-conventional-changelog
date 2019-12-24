7106df7c81882b5609778a31697dd41fe9aec1d0
amaury-tobias <isidro.tobias@prestamofeliz.com.mx>
2019-12-24 12:53:40 -0600

    refactor(index): 💡 add default config, delete auto invoiced function
    BREAKING CHANGE: 🧨Now needs to declare `maxHeaderWidth` y configuration file

--- index.js
+++ index.js
@@ -4,7 +4,7 @@ var engine = require('./engine');
 var conventionalCommitTypes = require('conventional-commit-types');
 var configLoader = require('commitizen').configLoader;
 
-var config = configLoader.load();
+var config = configLoader.load() || {};
 var options = {
   emojis: config.emojis || false,
   types: config.types || conventionalCommitTypes.types,
@@ -24,24 +24,5 @@ var options = {
     config.maxLineWidth ||
     100
 };
-
-(function(options) {
-  try {
-    var commitlintLoad = require('@commitlint/load');
-    commitlintLoad().then(function(clConfig) {
-      if (clConfig.rules) {
-        var maxHeaderLengthRule = clConfig.rules['header-max-length'];
-        if (
-          typeof maxHeaderLengthRule === 'object' &&
-          maxHeaderLengthRule.length >= 3 &&
-          !process.env.CZ_MAX_HEADER_WIDTH &&
-          !config.maxHeaderWidth
-        ) {
-          options.maxHeaderWidth = maxHeaderLengthRule[2];
-        }
-      }
-    });
-  } catch (err) {}
-})(options);
 
 module.exports = engine(options);
